05ac8ce01dda4590bfeda7152d9815e6d31dbbe1
Billlynch <llynch.bill@gmail.com>
2016-07-31 21:24:48 +0100

    cleaned up the functions

--- python/updateDoorStatus.py
+++ python/updateDoorStatus.py
@@ -3,12 +3,12 @@ import MySQLdb
 # define what to do if a door is open (1 = closed, 0 = open)
 def updateDoorStatus(state,door):
   # connect to the database
-  db = MySQLdb.connect("localhost","USERNAME","PASSWORD","DATABASE")
+  db = MySQLdb.connect("mysql://107.180.26.64","door","1qazxsw2","door")
   # setup cursor
   cursor = db.cursor()
   try:
     # create the sql query to update the database for the correct state and door
-    sql = "UPDATE door_status SET door_status="+str(state)+" WHERE door_id=" + str(door)
+    sql = "UPDATE door SET d_state="+str(state)+" WHERE d_id=" + str(door)
     # exectute the sql
     cursor.execute(sql)
     # save the changed
