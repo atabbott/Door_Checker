d07249bb8a1c9f8d0a2281bf88ec91a47cffb443
Billlynch <llynch.bill@gmail.com>
2016-07-31 14:03:31 +0100

    setup android project

---
+++ Android_app/garage_door/app/src/main/java/com/billlynch/garagedoor/Connect.java
@@ -0,0 +1,45 @@
+package com.billlynch.garagedoor;
+import java.sql.*;
+
+/**
+ * Created by Bill on 29/07/16.
+ */
+public class Connect{
+
+    static Connection con = null;
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test(){
+        connect();
+    }
+
+    public static Boolean connect(){
+        final String host = "jdbc:mysql://localhost/garage_door";
+        final String username = "piDoor";
+        final String password = "1234";
+
+        try{
+            Class.forName("COM.MYSQL.JDBC.DRIVER");
+            con = DriverManager.getConnection(host,username,password);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        if (con == null){
+            System.out.println("not connected");
+        }
+        else{
+            System.out.println("Connected");
+        }
+
+
+        return true;
+    }
+}
