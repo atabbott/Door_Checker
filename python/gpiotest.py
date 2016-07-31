e467802f029ec66c3ef12ec30314a5d7ec12c332
Billlynch <llynch.bill@gmail.com>
2016-07-31 14:28:47 +0100

    swapped the readout

--- python/gpiotest.py
+++ python/gpiotest.py
@@ -13,18 +13,18 @@ while True:
   if leftDoor.is_pressed:
     if leftDoorState == False:
       leftDoorState = True
-      print("door one opened")
+      print("door one closed")
   else:
     if leftDoorState == True:
       leftDoorState = False
-      print("door one closed")
+      print("door one opened")
 
   # door 2
   if rightDoor.is_pressed:
     if rightDoorState == False:
       rightDoorState = True
-      print("door two opened")
+      print("door two closed")
   else:
     if rightDoorState == True:
       rightDoorState = False
-      print("door two closed")
+      print("door two opened")
